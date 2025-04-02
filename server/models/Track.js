f7f9a5b8e384f2c3ee17f172df5cf3700e48b892
KiTu <akitudoutel24@gmail.com>
2025-04-02 18:28:25 -0400

    Update Track.js

--- server/models/Track.js
+++ server/models/Track.js
@@ -1,29 +1,26 @@
-class Track {
-    static async getAll() {
-      const sql = `
-        SELECT t.*, u.username as artist_name 
-        FROM tracks t
-        JOIN users u ON t.artist_id = u.id
-        ORDER BY t.release_date DESC
-      `;
-      return await query(sql);
-    }
-  
-    static async getById(id) {
-      const sql = `
-        SELECT t.*, u.username as artist_name 
-        FROM tracks t
-        JOIN users u ON t.artist_id = u.id
-        WHERE t.id = ?
-      `;
-      const [track] = await query(sql, [id]);
-      return track;
-    }
-  
-    static async incrementStream(trackId) {
-      const sql = 'UPDATE tracks SET stream_count = stream_count + 1 WHERE id = ?';
-      await query(sql, [trackId]);
-    }
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Track = sequelize.define('Track', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  duration: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  genre: {
+    type: DataTypes.STRING
+  },
+  stream_count: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  file_path: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
-  
-  module.exports = Track;
+});
+
+module.exports = Track;
